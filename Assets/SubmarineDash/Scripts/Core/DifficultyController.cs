b988e3eb8d695145b708f0ba0f8ed4c35044ca9c
InKres <Maxshalol1@mail.ru>
2025-08-04 21:31:26 +0500

    Added DifficultyController

---
+++ Assets/SubmarineDash/Scripts/Core/DifficultyController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+public class DifficultyController : MonoBehaviour
+{
+    public event Action<float> OnChangeDifficultyValue;
+
+    [Header("Settings")]
+    [SerializeField]
+    [Min(0f)]
+    private float startValue = 1f;
+    [SerializeField]
+    [Min(0f)]
+    private float increasePerSecond = 0.01f;
+
+    [Header("READ ONLY")]
+    [SerializeField]
+    private float m_CurrentDifficultyValue;
+    public float CurrentDifficultyValue
+    {
+        get
+        {
+            return m_CurrentDifficultyValue;
+        }
+        private set
+        {
+            m_CurrentDifficultyValue = value;
+        }
+    }
+
+    private bool isCanChangingDifficulty;
+
+    private void Update()
+    {
+        ChangeDifficulty();
+    }
+
+    public void Init(float difficultyValue = 0)
+    {
+        CurrentDifficultyValue = startValue;
+
+        if (difficultyValue > 0)
+        {
+            CurrentDifficultyValue = difficultyValue;
+        }
+    }
+
+    public void Dispose()
+    {
+        DisableChangingDifficulty();
+    }
+
+    private void ChangeDifficulty()
+    {
+        if (isCanChangingDifficulty == false)
+            return;
+
+        CurrentDifficultyValue += increasePerSecond * Time.deltaTime;
+        OnChangeDifficultyValue?.Invoke(CurrentDifficultyValue);
+    }
+
+    public void EnableChangingDifficulty()
+    {
+        isCanChangingDifficulty = true;
+    }
+
+    public void DisableChangingDifficulty()
+    {
+        isCanChangingDifficulty = false;
+    }
+}
