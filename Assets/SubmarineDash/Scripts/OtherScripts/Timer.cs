d95ebceea12afe4b9918cfc8097338413f9f62bf
InKres <Maxshalol1@mail.ru>
2025-08-28 22:29:58 +0500

    Added Timer script

---
+++ Assets/SubmarineDash/Scripts/OtherScripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Timer
+{
+    public IEnumerator TimerCoroutine(int seconds, Action OnComplited, Action<int> Countdown = null)
+    {
+        int secondCounter = seconds;
+
+        while (secondCounter > 0)
+        {
+            if (Countdown != null)
+                Countdown?.Invoke(secondCounter);
+
+            secondCounter--;
+
+            yield return new WaitForSeconds(1f);
+        }
+
+        OnComplited?.Invoke();
+    }
+}
